c64b0f09b86795a674a1ea1ef3cd95f9ae2093dd
LikePencil <slim8020@gmail.com>
2021-05-27 12:57:38 +0900

    Add Component Examples

--- src/App.js
+++ src/App.js
@@ -4,14 +4,38 @@ import React from 'react';
 import TodaysPlan from './Components/TodaysPlan';
 import MyComponent from './Components/MyComponent';
 import PropComponent from './Components/PropsComponent';
+import ChildComponent from './Components/ChildComponent';
+import BooleanComponent from './Components/BooleanComponent';
+import ChildComponent2 from './Components/ChildComponent2';
+import DefaultPropsComponent from './Components/DefaultPropsComponent';
 
 class App extends React.Component{
   render(){
+    const array = [1,2,3];
+    const obj = { name: '제목', age: 30};
+    const node = <h1>노드</h1>;
+    const func = () => { console.log('메시지');};
     return (
       <div className="body">
           <TodaysPlan/>
           <MyComponent name="Hello"/>
           <PropComponent name="두잇 리액트"/>
+          <ChildComponent
+            boolValue = {true}
+            numValue = {1}
+            arrayValue = {array}
+            objValue = {obj}
+            nodeValue = {node}
+            funcValue = {func}
+          />
+          <div>
+            <div><b>지루할 때:</b><BooleanComponent bored/></div>
+            <div><b>즐거울 때:</b><BooleanComponent/></div>
+          </div>
+          <div>
+            <ChildComponent2 objValue={{age: 20}} requiredStringValue="문자" />
+          </div>
+          <DefaultPropsComponent/>
       </div>
     );
   }
